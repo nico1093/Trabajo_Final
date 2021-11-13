8270623e86e2dc03d011a66a5a99a36e5e37b3e2
Lucas <Lucas@DESKTOP-DS60ULE>
2021-11-13 18:36:12 -0300

    Pusheo los estados y modos de la app

---
+++ src/main/java/Automatico.java
@@ -0,0 +1,22 @@
+package main.java;
+
+public class Automatico implements ModoDeApp {
+
+
+	@Override
+	public void conductorCambioDeCaminarAConducir(App app) {
+		if(app.isSeInicioEstacionamiento()) {
+			app.finalizarEstacionamiento();
+		}
+		
+	}
+
+	@Override
+	public void conductorCambioDeConducirACaminar(App app) {
+		if(app.seEncuentraEnUnaZonaEstacionamiento() && !app.isSeInicioEstacionamiento()) {
+			app.inicarEstacionamiento();
+		}
+	}
+
+
+}
---
+++ src/main/java/Manual.java
@@ -0,0 +1,22 @@
+package main.java;
+
+public class Manual implements ModoDeApp {
+
+
+	@Override
+	public void conductorCambioDeCaminarAConducir(App app) {
+		if(app.isSeInicioEstacionamiento()) {
+			app.mostrarAlertaDeFinalizacionDeEstacionamiento();
+		}
+		
+	}
+
+	@Override
+	public void conductorCambioDeConducirACaminar(App app) {
+		if( app.seEncuentraEnUnaZonaEstacionamiento() && !app.isSeInicioEstacionamiento()) {
+			app.mostrarAlertaDeFinalizacionDeEstacionamiento();
+		}
+		
+	}
+
+}
