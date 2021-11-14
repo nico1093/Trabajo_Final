0495f79dfea91d6363d69f1888063b3a2b30e1b5
luly_ <luly_@192.168.0.12>
2021-11-14 05:38:02 -0300

    metodos de infraccion metodos de inspector

--- src/main/java/Infraccion.java
+++ src/main/java/Infraccion.java
@@ -3,15 +3,52 @@ package main.java;
 import java.util.Date;
 
 public class Infraccion {
+	private SEM sem = SEM.getInstance();
     private String patente;
     private Date fechaOrigen;
-    private double monto;
-    private ZonaDeEstacionamiento zona;
-
-    public Infraccion(String patente, double monto, ZonaDeEstacionamiento zonaEncargada){
-        this.monto = monto;
+    private ZonaDeEstacionamiento zonaEs;
+   
+    public Infraccion(String patente, ZonaDeEstacionamiento zonaEs){
+        
         this.patente = patente;
         this.fechaOrigen = new Date();
-        this.zona = zonaEncargada;
+        this.zonaEs= zonaEs;
+        
     }
+    
+    	
+	public String getPatente() {
+		return patente;
+	}
+
+	public void setPatente(String patente) {
+		this.patente = patente;
+	}
+
+	public Date getFechaOrigen() {
+		return fechaOrigen;
+	}
+
+	public void setFechaOrigen(Date fechaOrigen) {
+		this.fechaOrigen = fechaOrigen;
+	}
+	
+	 public SEM getSem() {
+	        return sem;
+	    }
+
+
+	public ZonaDeEstacionamiento getZonaEs() {
+		return zonaEs;
+	}
+
+
+	public void setZonaEs(ZonaDeEstacionamiento zonaEs) {
+		this.zonaEs = zonaEs;
+	}
+	 
+	 
+
+	
+	
 }
--- src/main/java/Inspector.java
+++ src/main/java/Inspector.java
@@ -1,14 +1,18 @@
 package main.java;
 
 public class Inspector {
+	private String nombreInspector;
     private final SEM sem = SEM.getInstance();
     private ZonaDeEstacionamiento zonaEncargada;
 
-    public Inspector(ZonaDeEstacionamiento zonaEncargada){
+    public Inspector(String nombre,ZonaDeEstacionamiento zonaEncargada){
         this.zonaEncargada = zonaEncargada;
+        this.setNombreInspector(nombre);
     }
 
-    public void inspeccionarEstacionamiento(String patente){
+    
+
+	public void inspeccionarEstacionamiento(String patente){
         /**
          * Este mensaje verificara si el estacionamiento se encuentra en validez. En el caso que no se encuentre
          * valido este generara una infraccion.
@@ -18,4 +22,23 @@ public class Inspector {
         }
     }
 
+	public String getNombreInspector() {
+		return nombreInspector;
+	}
+
+	public void setNombreInspector(String nombreInspector) {
+		this.nombreInspector = nombreInspector;
+	}
+	
+	public ZonaDeEstacionamiento getZonaEncargada() {
+		return zonaEncargada;
+	}
+
+	public void setZonaEncargada(ZonaDeEstacionamiento zonaEncargada) {
+		this.zonaEncargada = zonaEncargada;
+	}
+
+	 public SEM getSem() {
+	        return sem;
+	    }
 }
