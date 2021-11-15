6d7eeaf577fd60ba06d2926022012b99656af783
Lucas <Lucas@DESKTOP-DS60ULE>
2021-11-14 22:14:36 -0300

    Borre un paquete de mas

--- src/src/main/java/EstadoDeMovimiento.java
+++
@@ -1,9 +0,0 @@
-package main.java;
-
-public interface EstadoDeMovimiento {
-	
-	public void llegoMensajeDriving(App app);
-	
-	public void llegoMensajeWalking(App app);
-
-}
--- src/src/main/java/ModoDeApp.java
+++
@@ -1,9 +0,0 @@
-package main.java;
-
-public interface ModoDeApp {
-
-	public abstract void conductorCambioDeCaminarAConducir(App app);
-	
-	public abstract void conductorCambioDeConducirACaminar(App app);
-	
-}
