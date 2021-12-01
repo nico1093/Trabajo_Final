0460f5fb92866f540e6c742eb38d44fb0d13b62b
Nicolas Gustavo Martin <NMARTIN@gire.com>
2021-12-01 11:37:24 -0300

    Actualizado la version, se borraron clases adicionales que se encontraban en la version anterior

--- src/main/java/ZonaDeEstacionamiento.java
+++
@@ -1,8 +0,0 @@
-import java.util.List;
-
-public class ZonaDeEstacionamiento {
-    private SEM sem = SEM.getInstance();
-    private Inspector encargado;
-    private List<Comercio> comercios;
-    private List<Estacionamiento> estacionados;
-}
